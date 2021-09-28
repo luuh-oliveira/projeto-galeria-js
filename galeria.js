21b013114d4f8677c933a4d6f897fb8e23b23035
luuh-oliveira <lu.oliveira0505@gmail.com>
2021-09-28 11:29:02 -0300

    Inserção de slides

--- galeria.js
+++ galeria.js
@@ -11,6 +11,8 @@ const imagens = [
     "./img/img8_toquio.jpg"
 ]
 
+const getId = (urlImagem) => urlImagem.split("/")[2].split(".")[0].toLowerCase()
+
 const criarItem = (urlImagem) => {
     const container = document.querySelector(".galeria-container")
     // container.innerHTML += `
@@ -19,12 +21,43 @@ const criarItem = (urlImagem) => {
     //     </a>
     // `
     const novoLink = document.createElement("a")
-    novoLink.href = urlImagem
+    novoLink.href = `#${getId(urlImagem)}`
     novoLink.classList.add("galeria-itens")
     novoLink.innerHTML = `<img src="${urlImagem}" alt=""></img>`
     container.appendChild(novoLink)
 }
 
+
+const criarSlide = (urlImagem, indice, arr) => {
+
+    const container = document.querySelector(".slide-container")
+
+    const slide = document.createElement("div")
+    slide.classList.add("slide")
+    slide.id = getId(urlImagem)
+
+    const indiceAnterior = indice > 0? indice -1 : arr.length - 1
+    const idAnterior = getId(arr[indiceAnterior])
+
+    const indiceProximo = indice < arr.length - 1 ? indice + 1 : 0
+    const idProximo = getId(arr[indiceProximo])
+
+    slide.innerHTML = `
+        <div class="img-container">
+            <a href="#" class="fechar">&#10006;</a>
+            <a href="#${idAnterior}" class="navegacao anterior">&#171;</a>
+            <img src="${urlImagem}" alt="">
+            <a href="#${idProximo}" class="navegacao proximo">&#187;</a>
+        </div>
+    `
+
+    container.appendChild(slide)
+
+}
+
+
 const carregarImagens = () => imagens.forEach(criarItem)
+const carregarSlides = () => imagens.forEach(criarSlide)
 
 carregarImagens()
+carregarSlides()
